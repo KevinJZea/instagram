9a7c390c7f170ae0450b967f591c91e6c934bb0c
Kevin J. Zea <kevinjzea@gmail.com>
2023-11-25 14:14:00 -0600

    ➕ feat: Navbar component created and implemented

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,3 +8,6 @@ export const ICONS = {
   MENU: 'menu',
   SEARCH: 'search',
 };
+
+export const PROFILE_IMAGE_URL =
+  'https://lh3.googleusercontent.com/ogw/ADGmqu8hSa4xMOm3viY0tGpajS4YG3KInDt9dfov_zi8=s83-c-mo';
