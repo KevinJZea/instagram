ff43b2733b4db13fdcaad0609f9f875b6d47f4b3
Kevin J. Zea <kevinjzea@gmail.com>
2023-11-26 01:07:58 -0600

    ➕ feat: PostCard component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { Navbar } from './components/Navbar';
+import { PostCard } from './components/PostCard';
+import { data } from './utils/data';
 import './App.css';
 
 function App() {
   return (
     <>
       <Navbar />
-      <main className="Main"></main>
+      <main className="Main">
+        <div className="Feed">
+          <div className="StoriesContainer"></div>
+          <div className="PostsContainer">
+            {data.posts.map((post) => (
+              <PostCard
+                key={post.id}
+                {...post}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="Info"></div>
+      </main>
     </>
   );
 }
